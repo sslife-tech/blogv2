4ee4dd24bd844ee74a5d7945e1700b0f87b6691a
sawadashota <xiootas@gmail.com>
2020-09-22 16:30:47 +0900

    Introduce PWA (#11)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,8 +14,20 @@ class MyDocument extends Document {
             <Html lang='ja'>
                 <Head>
                     <link rel="icon" href="/favicon.png"/>
+                    <link rel='manifest' href='/manifest.json' />
                     <link rel="shortcut icon" href={`${config.baseURL}/favicon.png`}/>
                     <link rel='apple-touch-icon' href={`${config.baseURL}/images/apple-touch-icon.png`}/>
+                    <meta name='application-name' content='SSLife Tech' />
+                    <meta name='apple-mobile-web-app-capable' content='yes' />
+                    <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+                    <meta name='apple-mobile-web-app-title' content='SSLife Tech' />
+                    <meta name='description' content='しょうちゃんとしおりんのブログ' />
+                    <meta name='format-detection' content='telephone=no' />
+                    <meta name='mobile-web-app-capable' content='yes' />
+                    <meta name='msapplication-TileColor' content='#0070f3' />
+                    <meta name='msapplication-tap-highlight' content='no' />
+                    <meta name='theme-color' content="#0070f3"/>
+                    <meta property="og:site_name" content="SSLife Tech"/>
                 </Head>
                 <body>
                 <Main/>
