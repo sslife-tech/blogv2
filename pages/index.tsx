f927ff6c97924ad2ffc7860682beddb2ec74c917
sawadashota <shota@sslife.tech>
2020-09-22 13:56:57 +0900

    fix: Duplicated meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import {getSortedPosts, PostSummary} from "~/lib/posts";
 import {DefaultLayout} from "~/components/layouts/DefaultLayout";
 import {PostList} from "~/components/organisms/PostList";
 import style from '~/styles/Home.module.css'
+import Head from "next/head";
+import config from "~/Configuration";
 
 type Props = {
   posts: PostSummary[];
@@ -11,6 +13,11 @@ type Props = {
 
 const Home: NextPage<Props> = ({posts}) => (
     <DefaultLayout>
+        <Head>
+            <title>SSLife Tech</title>
+            <meta name="description" content="しょうちゃんとしおりんのブログ"/>
+            <meta content={`${config.baseURL}/ogp.jpg`} property="og:image"/>
+        </Head>
         <main className={style.main}>
             <PostList posts={posts}/>
         </main>
