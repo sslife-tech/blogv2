bd525dab599829d10d000097aef44d9774a8fc1a
sawadashota <xiootas@gmail.com>
2020-09-22 15:51:19 +0900

    feat: Image lazy load (#10)

--- components/layouts/DefaultLayout.tsx
+++ components/layouts/DefaultLayout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import {Header} from "~/components/organisms/Header";
 
 type Props = {
@@ -7,6 +8,11 @@ type Props = {
 
 const DefaultLayout: React.FunctionComponent<Props> = ({children}: Props) => (
     <>
+        <Head>
+            <meta name='viewport' content="width=device-width,initial-scale=1,minimum-scale=1"/>
+            <meta name='theme-color' content="#0070f3"/>
+            <meta property="og:site_name" content="SSLife Tech"/>
+        </Head>
         <Header/>
         {children}
         <footer style={{textAlign: 'center', padding: '12px 0', marginTop: '120px'}}>
