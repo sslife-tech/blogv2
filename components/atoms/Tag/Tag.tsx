231892c25937842abe468de884f0c19a5f34e13c
sawadashota <xiootas@gmail.com>
2020-11-07 18:23:09 +0900

    Fix Sentry (#30)

--- components/atoms/Tag/Tag.tsx
+++ components/atoms/Tag/Tag.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import style from './Tag.module.css';
+import * as Sentry from "@sentry/node";
 
 type Props = {
     text: string;
 }
 
 const Tag: React.FunctionComponent<Props> = ({text}) => (
-    <span className={style.tag}>
+    <span className={style.tag} onClick={() => Sentry.captureException(new Error("test aaa"))}>
         #{text}
     </span>
 );
