cb769db0c68d04c2d31e902ccd0622ae3fe787ea
sawadashota <shota@sslife.tech>
2020-11-07 16:44:04 +0900

    Fix Sentry

--- next.config.js
+++ next.config.js
@@ -1,9 +1,34 @@
 const withPlugins = require('next-compose-plugins');
 const withPWA = require("next-pwa")
 const runtimeCaching = require('next-pwa/cache')
-const SentryCliPlugin = require('@sentry/webpack-plugin');
+const withSourceMaps = require('@zeit/next-source-maps');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
-const isProd = process.env.NODE_ENV === "production";
+const {
+    NODE_ENV,
+    NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+    SENTRY_ORG,
+    SENTRY_PROJECT,
+    SENTRY_AUTH_TOKEN,
+    SENTRY_ENVIRONMENT,
+    VERCEL_GITHUB_COMMIT_SHA,
+} = process.env;
+
+const isProd = NODE_ENV === "production";
+
+const COMMIT_SHA = VERCEL_GITHUB_COMMIT_SHA;
+process.env.SENTRY_DSN = SENTRY_DSN;
+
+let nextEnv = {};
+if (!!SENTRY_ENVIRONMENT) {
+    nextEnv = {
+        SENTRY_DSN,
+        SENTRY_ORG,
+        SENTRY_PROJECT,
+        COMMIT_SHA,
+        SENTRY_ENVIRONMENT,
+    }
+}
 
 module.exports = withPlugins(
     [
@@ -13,16 +38,54 @@ module.exports = withPlugins(
                 dest: "public",
                 runtimeCaching
             }
-        })
+        }),
+        withSourceMaps({
+            webpack(config, options) {
+                // In `pages/_app.js`, Sentry is imported from @sentry/browser. While
+                // @sentry/node will run in a Node.js environment. @sentry/node will use
+                // Node.js-only APIs to catch even more unhandled exceptions.
+                //
+                // This works well when Next.js is SSRing your page on a server with
+                // Node.js, but it is not what we want when your client-side bundle is being
+                // executed by a browser.
+                //
+                // Luckily, Next.js will call this webpack function twice, once for the
+                // server and once for the client. Read more:
+                // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+                //
+                // So ask Webpack to replace @sentry/node imports with @sentry/browser when
+                // building the browser's bundle
+                if (!options.isServer) {
+                    config.resolve.alias['@sentry/node'] = '@sentry/browser';
+                }
+
+                // When all the Sentry configuration env variables are available/configured
+                // The Sentry webpack plugin gets pushed to the webpack plugins to build
+                // and upload the source maps to sentry.
+                // This is an alternative to manually uploading the source maps
+                // Note: This is disabled in development mode.
+                if (
+                    SENTRY_DSN &&
+                    SENTRY_ORG &&
+                    SENTRY_PROJECT &&
+                    SENTRY_AUTH_TOKEN &&
+                    COMMIT_SHA
+                ) {
+                    config.plugins.push(
+                        new SentryWebpackPlugin({
+                            include: '.next',
+                            ignore: ['node_modules'],
+                            urlPrefix: '~/_next',
+                            release: COMMIT_SHA,
+                        })
+                    );
+                }
+
+                return config;
+            }
+        }),
     ],
     {
-        plugins: [
-            new SentryCliPlugin({
-                include: '.',
-                ignoreFile: '.sentrycliignore',
-                ignore: ['node_modules', 'webpack.config.js'],
-                configFile: 'sentry.properties',
-            }),
-        ],
+        env: nextEnv,
     }
 )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,13 @@ import {AppProps} from "next/app";
 import '~/styles/reset.css';
 import '~/styles/globals.css';
 import Head from "next/head";
-import * as Sentry from "@sentry/browser";
+import * as Sentry from '@sentry/node';
 import { Integrations } from "@sentry/tracing";
 
-if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+if (process.env.SENTRY_DSN) {
     Sentry.init({
-        dsn: "https://80c42edba5d34fc59884a8c273be896c@o473167.ingest.sentry.io/5507831",
+        dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-        // To set your release version
-        release: "blogv2@" + process.env.npm_package_version,
         integrations: [new Integrations.BrowserTracing()],
 
         // Set tracesSampleRate to 1.0 to capture 100%
@@ -19,6 +17,7 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
         // We recommend adjusting this value in production
         tracesSampleRate: 1.0,
 
+        environment: process.env.SENTRY_ENVIRONMENT,
     });
 }
 
@@ -30,7 +29,6 @@ const app = ({Component, pageProps}: AppProps): JSX.Element => (
         </Head>
         <Component {...pageProps} />
     </>
-
 );
 
 export default app;
